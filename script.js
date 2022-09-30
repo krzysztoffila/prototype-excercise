f765c9a7052c3998c15222c9b453915bbb8bb0d2
krzysztoffila <umowy.fila@gmail.com>
2022-09-30 13:35:55 +0200

    Move engineStatus's to new onject vehicleActions

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+const vehicleActions = {
+    startEngine() {
+        this.engineStatus = 'on';
+        console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
+    },
+    stopEngine() {
+        this.engineStatus = 'off';
+        console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
+    }
+}
+
 function Vehicle(name, color) {
     const car = {};
     car.name = name;
     car.color = color;
     car.engineStatus = 'off';
-    //bedziemy operować na metodach starEngine, stopEngine
-    car.startEngine = function () {
-        this.engineStatus = 'on';
-        console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
-    }
-    car.stopEngine = function () {
-        this.engineStatus = 'off';
-        console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
-    }
+
     return car;
 }
 const car1 = Vehicle('Mustang', 'red');
 console.log(car1); // bez returna mamy undefined
+const car2 = Vehicle('Camaro', 'yellow');
+console.log(car2);
