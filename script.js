55eb4a94e826b2917f2e83a330016fabdba33b58
krzysztoffila <umowy.fila@gmail.com>
2022-09-30 13:27:28 +0200

    Add object to function Vechice

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const car = {};
-car.name = 'Mustang';
-car.color = 'red';
-car.engineStatus = 'off';
-//bedziemy operować na metodach starEngine, stopEngine
-car.startEngine = function () {
-    this.engineStatus = 'on';
-    console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
+function Vehicle() {
+    const car = {};
+    car.name = 'Mustang';
+    car.color = 'red';
+    car.engineStatus = 'off';
+    //bedziemy operować na metodach starEngine, stopEngine
+    car.startEngine = function () {
+        this.engineStatus = 'on';
+        console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
+    }
+    car.stopEngine = function () {
+        this.engineStatus = 'off';
+        console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
+    }
 }
-car.stopEngine = function () {
-    this.engineStatus = 'off';
-    console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
-}
+
 console.log(car.engineStatus);
 car.stopEngine();
