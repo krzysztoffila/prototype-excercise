f918db18d452c13de1d2780322d5c8f22737fedb
krzysztoffila <umowy.fila@gmail.com>
2022-09-30 13:44:36 +0200

    Change object vehicleActions to Vehicle.prototype - Must be -> ... = function(){}

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const vehicleActions = {
-    startEngine() {
-        this.engineStatus = 'on';
-        console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
-    },
-    stopEngine() {
-        this.engineStatus = 'off';
-        console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
-    }
-}
-
 function Vehicle(name, color) {
     const car = Object.create(vehicleActions);
     car.name = name;
@@ -17,6 +6,18 @@ function Vehicle(name, color) {
 
     return car;
 }
+Vehicle.prototype.startEngine = function () {
+    this.engineStatus = 'on';
+    console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
+};
+Vehicle.prototype.stopEngine = function () {
+    this.engineStatus = 'off';
+    console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
+};
+
+
+console.log(Vehicle.prototype);
+
 const car1 = Vehicle('Mustang', 'red');
 console.log(car1); // bez returna mamy undefined
 const car2 = Vehicle('Camaro', 'yellow');
