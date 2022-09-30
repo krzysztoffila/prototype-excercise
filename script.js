dac7fae984510265013713ceb5e825ba139a0cf0
krzysztoffila <umowy.fila@gmail.com>
2022-09-30 13:25:30 +0200

    Create methods startEngine, stopEngine

---
+++ script.js
@@ -0,0 +1,15 @@
+const car = {};
+car.name = 'Mustang';
+car.color = 'red';
+car.engineStatus = 'off';
+//bedziemy operować na metodach starEngine, stopEngine
+car.startEngine = function () {
+    this.engineStatus = 'on';
+    console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
+}
+car.stopEngine = function () {
+    this.engineStatus = 'off';
+    console.log(`Engine of a ${this.name} is ${this.engineStatus}`);
+}
+console.log(car.engineStatus);
+car.stopEngine();
